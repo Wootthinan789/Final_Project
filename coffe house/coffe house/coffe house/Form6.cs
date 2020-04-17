cb64d5193d3b918b8dde3fd8d435d7be63f9e7c9
warayut <warayut.a@ku.th>
2020-04-17 20:13:15 +0700

    edit code for menu

--- coffe house/coffe house/coffe house/Form6.cs
+++ coffe house/coffe house/coffe house/Form6.cs
@@ -15,6 +15,7 @@ namespace coffe_house
         public Form6()
         {
             InitializeComponent();
+           // groupBox1 = groupBox1;
         }
 
         private void pictureBox2_Click(object sender, EventArgs e)
