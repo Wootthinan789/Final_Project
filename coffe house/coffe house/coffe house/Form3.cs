2c21fb82c9c5796d19f9c4e9012d7a43f66de845
warayut <warayut.a@ku.th>
2020-04-19 10:42:32 +0700

    edit error base

--- coffe house/coffe house/coffe house/Form3.cs
+++ coffe house/coffe house/coffe house/Form3.cs
@@ -14,6 +14,7 @@ namespace coffe_house
 {
     public partial class Form3 : Form
     {
+        static string[] mamo = {"0","1"};
         static int num = 1;
         public Form3()
         {
@@ -64,23 +65,48 @@ namespace coffe_house
 
         public void pictureBox6_Click(object sender, EventArgs e)
         {
-            Form5 form5 = new Form5();
-            form5.Show();
+            //Form5 form5 = new Form5();
+            //form5.Show();
 
             string sql = "SELECT * FROM `sales`";
-            sql = "INSERT INTO sales (SaleID,SaleDateTime,CustomerID) VALUES ('16','2020-04-03 05:30:19','10')"; //VALUES ('"+text.text+"','"+text.text+"','"+text.text+"')
+            //sql = "INSERT INTO sales (SaleID,SaleDateTime,CustomerID) VALUES ('16','2020-04-03 05:30:19','10')"; //VALUES ('"+text.text+"','"+text.text+"','"+text.text+"')
             MySqlConnection con1 = new MySqlConnection("server=127.0.0.1;port=3306;username=test;password=12345678;database=testdata");
             MySqlCommand cmd = new MySqlCommand(sql,con1);
             
             con1.Open();
-            /*MySqlDataReader reader = cmd.ExecuteReader();
+            string c = "";
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<string> AuthorList = new List<string>();
             while (reader.Read())
             {
-                MessageBox.Show(reader.GetString("StaffID"));
-            }*/
 
-            cmd.ExecuteNonQuery();
-            con1.Close();
+                //label3.Text = (reader.GetString("SaleID"));
+                //MessageBox.Show(reader.GetString("SaleID"));
+                c += (reader.GetString("SaleID"));
+                AuthorList.Add(reader.GetString("SaleID"));
+            }
+            //cmd.ExecuteNonQuery();
+            //con1.Close();
+            /*
+            AuthorList.Add("Mahesh Chand");
+            AuthorList.Add("Praveen Kumar");
+            AuthorList.Add("Raj Kumar");
+            AuthorList.Add("Nipun Tomar");
+            AuthorList.Add("Dinesh Beniwal");
+            */
+            for (int i =0;i < AuthorList.Count; ++i)
+            {
+                if (textBox2.Text == AuthorList[i])
+                {
+                    MessageBox.Show("เจอ");
+                }
+                else
+                {
+                    
+                }
+                //label3.Text = AuthorList[i];
+                //MessageBox.Show(AuthorList[i]);
+            }
 
         }
 
